6d6a7dc8b2e7f93af526cb93df21aec498f853b9
kv2709 <kv2709$gmail.com>
2017-10-28 16:58:47 +0600

    Make BD structure and input data

--- api_app/admin.py
+++ api_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from api_app.models import Youtube
+from api_app.models import YoutubeAgeGroupAnalytics
+from api_app.models import YoutubeDeviceAnalytics
+from api_app.models import YoutubeSexAnalytics
+from api_app.models import YoutubeGeoAnalytics
+from api_app.models import YoutubeOSAnalytics
 
 # Register your models here.
+admin.site.register(Youtube)
+admin.site.register(YoutubeAgeGroupAnalytics)
+admin.site.register(YoutubeDeviceAnalytics)
+admin.site.register(YoutubeSexAnalytics)
+admin.site.register(YoutubeGeoAnalytics)
+admin.site.register(YoutubeOSAnalytics)
--- api_app/models.py
+++ api_app/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
 # Create your models here.
+class Youtube(models.Model):
+    name = models.CharField(max_length=255)
+    title = models.TextField(blank=True, null=True)
+    view_rate = models.DecimalField(default=0, max_digits=14, decimal_places=7)
+
+    def __str__(self):
+        """Return string value model"""
+        return str(self.name)
+
+class YoutubeGeoAnalytics(models.Model):
+    class Meta:
+        unique_together = (('youtube_channel', 'country_code', ),)
+    youtube_channel = models.ForeignKey(Youtube, related_name='country_views')
+    country_code = models.CharField(max_length=5)
+    viewer_percentage = models.DecimalField(default=0, max_digits=5, decimal_places=2)
+    def __str__(self):
+        """Return string value model"""
+        return "GeoAnalytics - "+ str(self.youtube_channel) + " - " + \
+            self.country_code + " - " + str(self.viewer_percentage)
+
+class YoutubeSexAnalytics(models.Model):
+    class Meta:
+        unique_together = (('youtube_channel', 'gender', ),)
+    youtube_channel = models.ForeignKey(Youtube, related_name='gender_views')
+    gender = models.CharField(max_length=10)
+    viewer_percentage = models.DecimalField(default=0, max_digits=5, decimal_places=2)
+    def __str__(self):
+        """Return string value model"""
+        return "SexAnalytics - " + str(self.youtube_channel) + " - " + \
+            self.gender + " - " + str(self.viewer_percentage)
+
+class YoutubeAgeGroupAnalytics(models.Model):
+    class Meta:
+        unique_together = (('youtube_channel', 'age_group', ),)
+    youtube_channel = models.ForeignKey(Youtube, related_name='age_views')
+    age_group = models.CharField(max_length=10)
+    viewer_percentage = models.DecimalField(default=0, max_digits=5, decimal_places=2)
+    def __str__(self):
+        """Return string value model"""
+        return "AgeGroupAnalytics " + str(self.youtube_channel) + " - " + \
+            self.age_group + " - " + str(self.viewer_percentage)
+
+class YoutubeDeviceAnalytics(models.Model):
+    class Meta:
+        unique_together = (('youtube_channel', 'device_type', ),)
+    youtube_channel = models.ForeignKey(Youtube, related_name='device_views')
+    device_type = models.CharField(max_length=50)
+    viewer_percentage = models.DecimalField(default=0, max_digits=5, decimal_places=2)
+    def __str__(self):
+        """Return string value model"""
+        return "DeviceAnalytics - " + str(self.youtube_channel) + " - " + \
+            self.device_type + " - " + str(self.viewer_percentage)
+
+class YoutubeOSAnalytics(models.Model):
+    class Meta:
+        unique_together = (('youtube_channel', 'os', ),)
+    youtube_channel = models.ForeignKey(Youtube, related_name='os_views')
+    os = models.CharField(max_length=50)
+    viewer_percentage = models.DecimalField(default=0, max_digits=5, decimal_places=2)
+    def __str__(self):
+        """Return string value model"""
+        return "OSAnalytics - " + str(self.youtube_channel) + " - " + \
+            self.os + " - " + str(self.viewer_percentage)
+
